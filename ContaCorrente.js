aa161190aafb50196cb4ea50ab6e3160b979be69
Vitor Ferraz <vitor_ferrazmaciel@hotmail.com>
2021-01-13 14:18:07 -0300

    Orientação a objeto

---
+++ ContaCorrente.js
@@ -0,0 +1,48 @@
+import { Cliente } from "./Cliente.js";
+
+export class ContaCorrente {
+    
+    agencia;
+    cliente;
+    
+    set cliente(novoValor){
+        if(novoValor instanceof cliente){
+        this.cliente = novoValor;
+    } 
+}
+
+    get cliente(){
+    return this.cliente;
+}
+
+    _saldo = 0;
+
+    get _saldo(){
+        return this.saldo;
+    }
+
+    constructor(cliente, agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
+    }
+
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) return;
+        this.saldo += valor;
+    }
+
+    transferir(valor, conta) {
+        const valoSacado = this.sacar(valor);
+        conta.depositar(valorSacado)
+    }
+}
+
+
